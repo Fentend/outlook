938310e3137727507bee4e416f4350faa5a8756d
Fentend <fentendmarkettech@gmail.com>
2023-12-19 14:12:48 +0100

    outlook mail improvised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './Component/Login';
+import Password from './Component/Password';
 
 function App() {
+ 
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Login/> */}
+      <Routes>
+      <Route path="/" Component={Login}/>
+      <Route path="/password" Component={Password}/>
+      </Routes>
     </div>
+
+    </BrowserRouter>
+
+
+
+
+
+   
+      
   );
+  
 }
 
 export default App;
